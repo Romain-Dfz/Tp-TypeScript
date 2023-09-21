65a9200f1bd5e17ad407fae8d9b6ff18042732a6
Romain Défossez <defossez.romain59@gmail.com>
2023-09-21 16:38:48 +0200

    tp-typescript

---
+++ js/main.js
@@ -0,0 +1,27 @@
+"use strict";
+let userName = "Romain";
+let userAge = 24;
+let isLogin = true;
+let userNames = [];
+userNames.push(userName);
+console.log(userNames);
+let person;
+person = {
+    firstName: "Dupont",
+    age: 20,
+    isLoggedIn: true,
+};
+// console.log(person.age);
+let person1 = {
+    firstName: "Dupont",
+    age: 20,
+    isLoggedIn: true,
+};
+console.log(person1.age);
+let person2 = {
+    firstName: "Dupont",
+    age: 20,
+    isLoggedIn: true,
+};
+console.log(person2.age);
+let infos = ["Romain", 24];
